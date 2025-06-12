65be169bc0c2f4a5f6ae28650ae87c2dd88b27dc
Zain <zunynali@gmail.com>
2025-06-12 22:59:40 +0500

    Add files via upload

--- Header.h
+++ Header.h
@@ -1,5 +1,6 @@
 #pragma once
 #include"raylib.h"
+#include"String.h"
 
 const int screen_width = 1400;
 const int screen_height = 800;
@@ -15,3 +16,24 @@ enum class ShipType { Destroyer, Submarine, Cruiser, Battleship, Carrier };
 enum class GameMode { P1vP2, PvAi_easy, PvAi_medium, PvAi_hard };
 
 
+struct GamePlayed {
+	int games_played;
+	int player_win;
+	int ai_wins;
+	int total_shots;
+	int total_hits;
+};
+
+struct Particle {
+	Vector2 pos;
+	Vector2 velocity;
+	Color color;
+	float size;
+};
+
+struct Animation {
+	Vector2 pos;
+	float time;
+	Color color;
+	String text;
+};
