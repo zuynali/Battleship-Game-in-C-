21fb14cd101153b6a92923e76828dcc560743c6c
Zain <zunynali@gmail.com>
2025-06-12 22:52:35 +0500

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,17 @@
+#pragma once
+#include"raylib.h"
+
+const int screen_width = 1400;
+const int screen_height = 800;
+const int grid_size = 10;
+const int cell_size = 45;
+const int board_x = 100;
+const int board_y = 150;
+const int menu = 300;
+
+enum class Cellstate { Empty, Ship, Hit, Miss, Skinking };
+enum class Gamestate { MainMenu, Ship_placement, P1_turn, P2_turn, AI_turn, Game_Over, Settings, Stats };
+enum class ShipType { Destroyer, Submarine, Cruiser, Battleship, Carrier };
+enum class GameMode { P1vP2, PvAi_easy, PvAi_medium, PvAi_hard };
+
+
